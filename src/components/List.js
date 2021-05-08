6279b217a1b0fd9d6b24ed1f2efd23d6bb32f194
teja0404 <tejailla0404@gmail.com>
2021-05-08 11:54:54 +0530

    Added state also

--- src/components/List.js
+++ src/components/List.js
@@ -4,10 +4,24 @@ import React from 'react';
 class List extends React.Component{
 
 
+    constructor(props){
+        super(props);
+
+        this.state={
+            name:props.text
+        }
+    }
+    
+    
+
     render(){
+
+        
+
         return(
             <div className="todo-item">
                 <input type="checkbox" />
+                <h1>{this.state.name}</h1>
                 <p>{this.props.text}</p>
             </div>
         );
